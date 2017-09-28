7b17e40a201788461e66ea474d59c9b22ead2d41
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-09-28 09:13:06 -0600

    unsaved change so im submitting.

--- src/practice/controller/PracticeRunner.java
+++ src/practice/controller/PracticeRunner.java
@@ -1,5 +1,10 @@
 package practice.controller;
 
-public class PracticeRunner {
-
+public class PracticeRunner 
+{
+	public static void main (String [] args)
+	{
+		PracticeController myApp = new PracticeController();
+		myApp.start();
+    }
 }
